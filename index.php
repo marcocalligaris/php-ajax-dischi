847afc339db89dad52be1eb048371fc7a38df247
marcocalligaris <marcocally@gmail.com>
2022-09-01 18:49:12 +0200

    Version 1 ok

--- index.php
+++ index.php
@@ -1,76 +1,5 @@
 <?php
-$discs = [
-        [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
-            'genre' => 'Rock'
-        ],
-        [
-            'title' => 'Live at Wembley 86',
-            'author' => 'Queen',
-            'year' => 1992,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Ten\'s Summoner\'s Tales',
-            'author' => 'Sting',
-            'year' => 1993,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Steve Gadd band',
-            'author' => 'Steve Gadd Band',
-            'year' => 2018,
-            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
-            'genre' => 'Jazz'
-        ],
-        [
-            'title' => 'Brave new World',
-            'author' => 'Iron Maiden',
-            'year' => 2000,
-            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
-            'genre' => 'Metal'
-        ],
-        [
-            'title' => 'One more car, one more rider',
-            'author' => 'Eric Clapton',
-            'year' => 2002,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
-            'genre' => 'Rock'
-        ],
-        [
-            'title' => 'Made in Japan',
-            'author' => 'Deep Purple',
-            'year' => 1972,
-            'poster' => 'https://m.media-amazon.com/images/I/71SxPkY9fKL._AC_SL1300_.jpg',
-            'genre' => 'Rock'
-        ],
-        [
-            'title' => 'And justice for all',
-            'author' => 'Metallica',
-            'year' => 1988,
-            'poster' => 'https://m.media-amazon.com/images/I/81pchxSIApL._AC_SL1200_.jpg',
-            'genre' => 'Metal'
-        ],
-        [
-            'title' => 'Hard wired',
-            'author' => 'Dave Weckl',
-            'year' => 1994,
-            'poster' => 'https://m.media-amazon.com/images/I/611TBSi2y8L._AC_SL1200_.jpg',
-            'genre' => 'Metal'
-        ],
-        [
-            'title' => 'Bad',
-            'author' => 'Michael Jackson',
-            'year' => 1987,
-            'poster' => 'https://m.media-amazon.com/images/I/51-c9KBefDL._AC_.jpg',
-            'genre' => 'Pop'
-        ],
-    ];
+include __DIR__ . '../includes/discs.php';
 
 ?>
 <!DOCTYPE html>
@@ -86,11 +15,31 @@ $discs = [
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header>
+    <!-- INTESTAZIONE -->
+    <header class="p-3">
         <div>
             <a href="https://open.spotify.com/"><img src="https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg" alt="Spotify logo"></a>
         </div>
     </header>
+    <!-- CORPO PRINCIPALE CON GALLERY -->
+    <main>
+        <div class="container py-5">
+            <div class="row g-4 d-flex flex-wrap">
+                <?php foreach ($discs as $disc) { ?>                
+                    <div class="column">
+                        <a class="text-decoration-none" href="#">
+                            <div class="disc-card p-3 text-center">
+                                <img class="w-100 mb-4" src="<?php echo $disc['poster'] ?>" alt="<?php echo $disc['title'] ?>">
+                                <h3 class="mb-4"><?php echo strtoupper($disc['title']) ?></h3>
+                                <h5><?php echo $disc['author'] ?></h5>
+                                <p><?php echo $disc['year'] ?></p>
+                            </div>
+                        </a>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
 
